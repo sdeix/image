b5dc70f2d15f56c9cc0daead8aa7616e9d76c7fa
sdeix <you@example.com>
2024-03-20 18:19:12 +0300

    comoser

---
+++ src/Image.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Image;
+
+class Image
+{
+   private array $array = [];
+
+   public function __construct(array $array = [])
+   {
+       $this->array = $array;
+   }
+
+   public function img($img)
+   {
+    $uploadfile= '';
+    if($img['size']>0){
+        $uploaddir = '../public/images/';
+        $uploadfile = $uploaddir . basename($img['name']);
+        if (move_uploaded_file($img['tmp_name'], $uploadfile)) {
+            $img = $uploadfile;
+        }
+    }
+    return $uploadfile;
+   }
+
+}
